117e5c9b92366c231298bc1f55186ba28683f6ae
Christian Dauz <christian.dauz@students.aie.edu.au>
2013-10-15 17:58:28 -0700

    Lightning Flash Script

---
+++ Christian Dauz/Lightning Storm.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var minTime = 0.5;
+var threshold = 0.5;
+var myLight = Light;
+
+private var lastTime = 0;
+
+function Update(){
+
+	if ((Time.time -lastTime) > minTime){
+	if (Random.value > threshold){
+	light.enabled = true;
+	}
+	else{
+	light.enabled = false;
+	}
+	}
+	lastTime = Time.time;
+}
